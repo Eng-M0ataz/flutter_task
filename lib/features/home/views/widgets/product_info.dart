9515d62cc7e00eaf6f55e7f4e5da2ec1b82e833c
Eng-Moataz <mizo201515@gmail.com>
2025-06-15 13:33:45 +0300

    refactor: make product info  widget accept product model  as parameter

--- lib/features/home/views/widgets/product_info.dart
+++ lib/features/home/views/widgets/product_info.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:task_porject/Features/home/data/models/product_model/product_model.dart';
 import 'package:task_porject/Features/home/views/widgets/add_to_cart_button.dart';
 import 'package:task_porject/Features/home/views/widgets/rating.dart';
 
 class ProductInfo extends StatelessWidget {
-  const ProductInfo({super.key});
-
+  const ProductInfo({super.key, required this.productModel});
+  final ProductModel productModel;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -12,14 +13,24 @@ class ProductInfo extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text("Product Name", maxLines: 1, overflow: TextOverflow.ellipsis),
           Text(
-            "Product Description",
+            productModel.title!,
             maxLines: 1,
             overflow: TextOverflow.ellipsis,
           ),
-          Text('price'),
-          Row(children: [RatingItem(), Spacer(), AddToCartButton()]),
+          Text(
+            productModel.description!,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Text('EGP ${productModel.price}'),
+          Row(
+            children: [
+              RatingItem(rating: productModel.rating!.rate!),
+              Spacer(),
+              AddToCartButton(),
+            ],
+          ),
         ],
       ),
     );
