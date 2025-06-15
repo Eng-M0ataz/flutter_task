5175150f9e6092014ace8223f6cfd1b19ce04e6b
Eng-Moataz <mizo201515@gmail.com>
2025-06-15 12:09:01 +0300

    implement api service class

---
+++ lib/core/services/api_service.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio _dio;
+  ApiService(this._dio);
+
+  Future<List<Map<String, dynamic>>> getReq({
+    required String endPoint,
+    required String baseUrl,
+    int? pageNumber,
+    String? token,
+    int retry = 3,
+  }) async {
+    Map<String, String> headers = {};
+    if (token != null) {
+      headers.addAll({
+        'Authorization': 'Bearer $token',
+        'accept': 'application/json',
+      });
+    }
+
+    Map<String, dynamic> queryParameters = {};
+    if (pageNumber != null) {
+      queryParameters.addAll({'page': pageNumber, 'language': 'en-US'});
+    } else {
+      queryParameters.addAll({'language': 'en-US'});
+    }
+
+    Response response = await _dio.get(
+      '$baseUrl$endPoint',
+      queryParameters: queryParameters,
+      options: Options(headers: headers),
+    );
+
+    return response.data;
+  }
+}
