7fb36eb18d50f395c7a9cc93457ffd90aa20b1b1
Eng-Moataz <mizo201515@gmail.com>
2025-06-15 13:56:42 +0300

    refactor: replace old grid view to grideview with bloc builder

--- lib/features/home/views/widgets/home_view_body.dart
+++ lib/features/home/views/widgets/home_view_body.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:task_porject/Features/home/views/widgets/product_grid_view.dart';
+import 'package:task_porject/Features/home/views/widgets/product_grid_view_bloc_bulder.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ProductGridView();
+    return ProductGridViewBlocBuilder();
   }
 }
