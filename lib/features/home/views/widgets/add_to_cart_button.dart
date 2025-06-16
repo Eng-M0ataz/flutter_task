ba8fab54dae04ae50b9bdaac93ecf445f81df294
Eng-Moataz <mizo201515@gmail.com>
2025-06-16 06:12:34 +0300

    refactore: add snackbar with message when a user interact with (add to cart button)

--- lib/features/home/views/widgets/add_to_cart_button.dart
+++ lib/features/home/views/widgets/add_to_cart_button.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:task_porject/core/utils/functions/snack_bar.dart';
 
 class AddToCartButton extends StatelessWidget {
   const AddToCartButton({super.key});
@@ -7,7 +8,12 @@ class AddToCartButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {},
+      onTap: () {
+        showSnackBar(
+          message: 'this product added to your cart successfully',
+          context: context,
+        );
+      },
       child: FaIcon(FontAwesomeIcons.circlePlus, color: Color(0xff004087)),
     );
   }
